ef5ea957e1790b5a1de201303b0a15adabbd093b
PettyaSt <petq_1002@abv.bg>
2022-12-20 17:17:07 +0200

    Create 04-pointDistance.js

---
+++ 08-Classes and Attributes/lab/04-pointDistance.js
@@ -0,0 +1,17 @@
+class Point {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static distance(point1, point2) {
+        let a = Math.pow(point1.x - point2.x, 2);
+        let b = Math.pow(point1.y - point2.y, 2);
+        let dist = Math.sqrt(a + b, 2);
+        return dist;
+    }
+}
+
+let p1 = new Point(5, 5);
+let p2 = new Point(9, 8);
+console.log(Point.distance(p1, p2));
